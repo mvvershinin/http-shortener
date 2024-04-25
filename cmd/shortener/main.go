0034f9d4c5b76ff3fd74983cf24d46a47f972b04
Вершинин Михаил <vershinin@benefitty.ru>
2024-04-26 01:40:54 +0700

    pr fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mvvershinin/http-shortener/internal/app/config"
 	"github.com/mvvershinin/http-shortener/internal/app/handler"
 	"net/http"
@@ -11,7 +10,7 @@ func main() {
 	cfg := config.GetConfig()
 	mux := http.NewServeMux()
 	mux.HandleFunc(`/`, handler.MainHandler)
-	fmt.Printf("listen %s\r\n", cfg.GetServerURL())
+	//fmt.Printf("listen %s\r\n", cfg.GetServerURL())
 	err := http.ListenAndServe(cfg.GetServerURL(), mux)
 	if err != nil {
 		panic(err)
