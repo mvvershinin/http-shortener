5dfa2d83e1e2b43335115399e1c8624594626738
Вершинин Михаил <vershinin@benefitty.ru>
2024-04-26 02:59:19 +0700

    pr fix

--- internal/app/strencoder/strencoder.go
+++ internal/app/strencoder/strencoder.go
@@ -7,9 +7,8 @@ import (
 
 func Base64Encode(str string) string {
 	enc := base64.StdEncoding.EncodeToString([]byte(str))
-	fmt.Printf("%s\n", enc)
+
 	return enc
-	//todo return err
 }
 
 func Base64Decode(str string) (string, error) {
@@ -18,7 +17,6 @@ func Base64Decode(str string) (string, error) {
 	if err != nil {
 		return str, err
 	}
-	fmt.Printf("%s\n", string(dec))
+
 	return string(dec), nil
-	//todo return err
 }
